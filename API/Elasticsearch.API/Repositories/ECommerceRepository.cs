49c5a641482a6fe8e86987d98f8d6c4886c12bd3
Hakan Bayazıt Habeş <hakanbayazithabes@gmail.com>
2024-08-09 11:57:39 +0300

    Refactored : Elasticsearch.API code to use Elastic.Clients.Elasticsearch

--- API/Elasticsearch.API/Repositories/ECommerceRepository.cs
+++ API/Elasticsearch.API/Repositories/ECommerceRepository.cs
@@ -1,5 +1,6 @@
 using System.Collections.Immutable;
 using Elastic.Clients.Elasticsearch;
+using Elastic.Clients.Elasticsearch.QueryDsl;
 using Elasticsearch.API.Models.ECommerce;
 
 namespace Elasticsearch.API.Repositories;
@@ -17,8 +18,38 @@ public class ECommerceRepository
 
     public async Task<ImmutableList<ECommerce>> TermQuery(string customerFirstName)
     {
+        /*
+        1. Yol
         var response = await _client.SearchAsync<ECommerce>(x => x.Index(indexName).Query(q => q.Term(t => t.Field("customer_first_name.keyword").Value(customerFirstName))));
+        
+        2. Yol
+        var response = await _client.SearchAsync<ECommerce>(s => s.Index(indexName).Query(q => q.Term(t => t.CustomerFirstName.Suffix("keyword"), customerFirstName)));
+        */
+
+        var termQuery = new TermQuery("customer_first_name.keyword") { Value = customerFirstName, CaseInsensitive = true };
+
+        var response = await _client.SearchAsync<ECommerce>(s => s.Index(indexName).Query(termQuery));
+
         response.Hits.ToList().ForEach(hit => hit.Source.Id = hit.Id);
         return response.Documents.ToImmutableList();
+    }
+
+    public async Task<ImmutableList<ECommerce>> TermsQuery(List<string> customerFirstNameList)
+    {
+        List<FieldValue> terms = [];
+        customerFirstNameList.ForEach(x =>
+        {
+            terms.Add(x);
+        });
+
+        var termsQuery = new TermsQuery()
+        {
+            Field = "customer_first_name.keyword",
+            Terms = new TermsQueryField(terms.AsReadOnly())
+        };
+
+        var result = await _client.SearchAsync<ECommerce>(s => s.Query(termsQuery));
+
+        return result.Documents.ToImmutableList();
     }
 }
